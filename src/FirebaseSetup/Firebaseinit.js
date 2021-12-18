2b06412fe6e5cf484bf6ef26d2ce731e87a79825
Aljoboyer <tanveeazmain@gmail.com>
2021-12-18 09:24:42 +0600

    d

--- src/FirebaseSetup/Firebaseinit.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "firebase/app";
-import firebaseConfig from "./FirebaseConfig";
-const initializationapp = () => {
-    return initializeApp(firebaseConfig);
-}
-
-export default initializationapp;
